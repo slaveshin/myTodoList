10afc3a04324bff499efcad3621faa7644358f8f
shin seung jae <shinseungjae1234@gmail.com>
2022-06-22 22:49:13 +0900

    coredata를 이용해서 구성

---
+++ myTodoList/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  myTodoList
+//
+//  Created by 신승재 on 2022/06/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
