ab0f36435a1b6b713e34fd3ac662741a6e3ff5bc
shin seung jae <shinseungjae1234@gmail.com>
2022-06-25 01:00:49 +0900

    데이터 파싱 후 출력완료, create시 서버에도 반영이 될 수 있도록 변경

--- myTodoList/ViewController.swift
+++ myTodoList/ViewController.swift
@@ -1,52 +1,46 @@
 //  Created by 신승재 on 2022/06/22.
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var ref: DatabaseReference!
+    
     
     @IBOutlet weak var tableView: UITableView!
-    /*
-    let tableView: UITableView = {
-        let table = UITableView()
-        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        return table
-    } ()*/
-    
-    private var models = [ToDoListItem]()
+
+    private var models = [Todos]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        ref = Database.database().reference()
         title = "Todo List"
-        // view.addSubview(tableView)
+        
         getAllItems()
         tableView.delegate = self
         tableView.dataSource = self
-        // tableView.frame = view.bounds
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
     }
     
-    @objc private func didTapAdd() {
+    
+    @IBAction func pushAddButton(_ sender: Any) {
         let alert = UIAlertController(title: "New Item", message: "Enter new item", preferredStyle: .alert)
-        
-        alert.addTextField(configurationHandler: nil)
-        alert.addAction(UIAlertAction(title: "Submit", style: .cancel, handler: { [weak self] _ in
-            guard let field = alert.textFields?.first, let text = field.text, !text.isEmpty else {
-                return
-            }
-            
-            self?.createItem(name: text)
-            
-        }))
-        present(alert, animated: true)
+               
+               alert.addTextField(configurationHandler: nil)
+               alert.addAction(UIAlertAction(title: "Submit", style: .cancel, handler: { [weak self] _ in
+                   guard let field = alert.textFields?.first, let text = field.text, !text.isEmpty else {
+                       return
+                   }
+                   
+                   self?.createItem(name: text)
+                   
+               }))
+               present(alert, animated: true)
     }
-    
     
     // tableView
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return models.count
     }
@@ -54,92 +48,64 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let model = models[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
-        let date = model.createdAt ?? Date()
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yy-MM-dd"
-        let convertDate = dateFormatter.string(from: date)
         
         cell.name.text = model.name
-        
-        cell.createAt.text = "Create At \(convertDate)"
-        
+        cell.createAt.text = model.createAt
+                
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        let sheet = UIAlertController(title: "Edit", message: nil, preferredStyle: .actionSheet)
-        let item = models[indexPath.row]
-        
-        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        sheet.addAction(UIAlertAction(title: "Edit", style: .default, handler: { [weak self] _ in
-            
-            let alert = UIAlertController(title: "Edit Item", message: "Edit your item", preferredStyle: .alert)
-            
-            alert.addTextField(configurationHandler: nil)
-            alert.textFields?.first?.text = item.name
-            alert.addAction(UIAlertAction(title: "Submit", style: .cancel, handler: { [weak self] _ in
-                guard let field = alert.textFields?.first, let newName = field.text, !newName.isEmpty else {
-                    return
-                }
-                
-                self?.updateItem(item: item, newName: newName)
-                
-            }))
-            self?.present(alert, animated: true)
-        }))
-        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { [weak self] _ in
-            self?.deleteItem(item: item)
-        }))
-        present(sheet, animated: true)
-    }
     
-    // coredata
+    // realtime database
     func getAllItems() {
-        do {
-            models = try context.fetch(ToDoListItem.fetchRequest())
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
+        ref.child("TodoList").getData(completion:  { error, snapshot in
+          guard error == nil else {
+            print(error!.localizedDescription)
+            return
+          }
+            
+            do {
+                let data = try JSONSerialization.data(withJSONObject: snapshot?.value as Any, options: [])
+                let decoder = JSONDecoder()
+                self.models = try decoder.decode([Todos].self, from: data)
+                DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
             }
-        }
-        catch {
-            // error 처리
-        }
+            catch let error {
+                // error 처리
+                print("---> error \(error.localizedDescription)")
+            }
+        });
     }
     
     func createItem(name: String) {
-        let newItem = ToDoListItem(context: context)
-        newItem.name = name
-        newItem.createdAt = Date()
-        do {
-            try context.save()
-            getAllItems()
-        }
-        catch {
-            
+        let newName = name
+        let nowDate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy년 MM월 dd일"
+        let convertDate = dateFormatter.string(from: nowDate)
+        
+        
+        // update database
+        let key = self.models.count
+        let newItem = ["createAt":convertDate, "name":newName]
+        let childUpdates = ["/TodoList/\(key)": newItem]
+        ref.updateChildValues(childUpdates)
+        getAllItems()
+
+    }
+        
+       
+    
+    // JSON Data Parsing
+    struct Todos: Codable {
+        let createAt:String
+        let name:String
+        var toDictionary: [String:Any] {
+            return ["name" : name, "createAt" : createAt]
         }
     }
     
-    func deleteItem(item: ToDoListItem) {
-        context.delete(item)
-        do {
-            try context.save()
-            getAllItems()
-        }
-        catch {
-            
-        }
-    }
-
-    func updateItem(item: ToDoListItem, newName: String) {
-        item.name = newName
-        do {
-            try context.save()
-            getAllItems()
-        }
-        catch {
-            
-        }
-    }
 }
 
